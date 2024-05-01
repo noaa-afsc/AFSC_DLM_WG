142192f22adcd7a16ea5f6b091a408eda80360c6
tribuzio <cindy.tribuzio@noaa.gov>
2024-05-01 10:14:07 -0800

    added complex category and figures

--- T6_prioritization/T6_ranking.R
+++ T6_prioritization/T6_ranking.R
@@ -21,12 +21,14 @@ qual_met <- T6_metrics %>%
   pivot_longer(cols = !c(fmp_area, species_name, species_group_code), names_to = "Metric", values_to = "Value")
 
 #weights
-wt <- read_sheet('https://docs.google.com/spreadsheets/d/13ScMnmVnbBW1pCsG1zq5XBSWPYPJM9ymDLrIGse1VMk/edit?usp=sharing')
+wt <- read_sheet('https://docs.google.com/spreadsheets/d/13ScMnmVnbBW1pCsG1zq5XBSWPYPJM9ymDLrIGse1VMk/edit?usp=sharing') %>% 
+  filter(keep == "Y") %>% 
+  select(!(keep))
 quan_wt <- wt %>% 
-  filter(Metric %nin% c("dominance", "T6_HCR_data", "edge_dist", "marketable", "Tier_jump")) %>% 
+  filter(Metric %nin% c("dominance", "T6_HCR_data", "edge_dist", "marketable", "Tier_jump", "complex")) %>% 
   select(!Description)
 qual_wt <- wt %>% 
-  filter(Metric %in% c("dominance", "T6_HCR_data", "edge_dist", "marketable", "Tier_jump")) %>% 
+  filter(Metric %in% c("dominance", "T6_HCR_data", "edge_dist", "marketable", "Tier_jump", "complex")) %>% 
   select(!Description)
 
 # Weight each metric ----
@@ -46,6 +48,25 @@ qual2 <- qual_met %>%
 
  wt_met <- quan2 %>% 
    bind_rows(qual2)
+ 
+ unq_key <- wt_met %>% 
+   select(fmp_area, species_name) %>% 
+   unique()
+ 
+ for (i in 1:nrow(unq_key)){ #this kicks out 8 grobs
+   lfmp <- unq_key[i,1]
+   lspec <- unq_key[i,2]
+   loop_dat <- wt_met %>%
+     filter(fmp_area == lfmp,
+            species_name == lspec)
+   lfig <- ggplot(loop_dat, aes(x = Metric, y = weight))+
+     geom_bar(stat = "identity")+
+     facet_grid(.~fmp_area+species_name)+
+     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+   ggsave(path = paste0(getwd(), "/T6_prioritization"), 
+          paste0(lfmp, lspec, ".png"),
+                 plot = lfig, dpi=600)
+ }
   
 # Ranking ----  
 ranks <- wt_met %>% 
